6eefe7d957ead860f8b16365bc1f2131392a9c9e
denniswegereef <denniswegereef@gmail.com>
2019-01-15 11:46:51 +0100

    added dynamic insights overview page

--- pages/story-overview.vue
+++ pages/story-overview.vue
@@ -1,25 +1,77 @@
 <template>
   <section class="container">
-    <h1>You ended the game this is the overview</h1>
+    <!-- header -->
+    <h2 class="intro">Je bent klaar! Scroll verder om jouw scenario te bekijken</h2>
+
     <div
       v-for="(questionAnswer, index) in $store.state.questionsAnswered"
       :key="index"
-      class="bullet">
-      <section>
-        <h1>{{ questionAnswer.id }}</h1>
-        <h4>{{ questionAnswer.q }}</h4>
-        <h5>{{ questionAnswer.a }}</h5>
-      </section>
+      class="overview-single">
+      <h2>{{ questionAnswer.q }}</h2>
+      <p>Jouw antwoord</p>
+      <h3>{{ questionAnswer.a }}</h3>
+
+      <div
+        v-if="checkAvailable(questionAnswer.id)"
+        class="insight">
+        <h2>{{ findInsight(questionAnswer.id) }}</h2>
+      </div>
     </div>
+
   </section>
 </template>
 
 <script>
-export default {}
+import json from 'static/insights.json'
+
+export default {
+  validate({ params, store }) {
+    // Validate if question exists otherwise send to error
+    if (!store.state.jsonData === 'empty') {
+      return true
+    }
+    return true
+  },
+  data() {
+    return {
+      json
+    }
+  },
+  methods: {
+    checkAvailable(id) {
+      if (this.json.insights.find(x => x.id === id)) {
+        return true
+      }
+      return false
+    },
+    findInsight(id) {
+      let find = this.json.insights.find(x => x.id === id)
+      return find.insight
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+.intro {
+  margin-bottom: 40px;
+}
 section {
   margin-bottom: 20px;
+}
+.insight {
+  height: 100px;
+  width: 100%;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    color: #fff;
+  }
+}
+
+.overview-single {
+  margin-bottom: 40px;
 }
 </style>
