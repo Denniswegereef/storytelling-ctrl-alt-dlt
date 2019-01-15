11eb48808fd4eb7d166c366cdf8b5b0ca6bb43d0
Jessie Mason <jessiemasonx@hotmail.com>
2019-01-15 13:11:30 +0100

    changed index

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
   <section class="container">
     <h1>Mag ik ook nee zeggen?</h1>
     <p>Welkom bij Mag Ik Ook Nee Zeggen. <br> Hier ga je door een verhaal, waar jij bepaalt wat er gebeurt. Je komt er achter hoe het is om als burger staande gehouden worden en snel te moeten reageren op verschillende situaties.</p>
-    <nuxt-link to="/explanation">Go to explanation</nuxt-link>
-    <bigButton :text="'start'"/>
+    <nuxt-link to="/explanation">
+      <bigButton :text="'start'"/>
+    </nuxt-link>
   </section>
 </template>
 
