931a6790730faa2b0f4f5d45e7500c9063828f65
denniswegereef <denniswegereef@gmail.com>
2019-01-16 13:17:54 +0100

    added view all weetjes ofzo hihihi

---
+++ pages/insights.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <smallHeader :header-text="'Wist je dat?'"/>
+    <section class="container">
+      <div
+        v-for="insight in insights.insights"
+        :key="insight.id">
+        <p>{{ insight.insight }}</p>
+        <hr>
+      </div>
+      <bigButton
+        :text="'Terug naar overzicht'"
+        :to="'/story-overview'"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import smallHeader from '~/components/small/smallHeader.vue'
+import bigButton from '~/components/small/bigButton.vue'
+import insights from '~/static/insights.json'
+
+export default {
+  components: {
+    smallHeader,
+    bigButton
+  },
+  data() {
+    return {
+      insights
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 0 var(--default-padding);
+}
+</style>
