765cf360b29a4574b10fcf859e57adf012737914
denniswegereef <denniswegereef@gmail.com>
2019-01-18 12:33:00 +0100

    back to normal

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container">
-    <h1>Mag ik ook nee zeggen??</h1>
+    <h1>Mag ik ook nee zeggen?</h1>
     <div class="bodyContainer">
       <p>Welkom bij Mag Ik Ook Nee Zeggen. <br> Hier ga je door een verhaal, waar jij bepaalt wat er gebeurt. Je komt er achter hoe het is om als burger staande gehouden worden en snel te moeten reageren op verschillende situaties.</p>
       <bigButton
