8bbcee533aad5666c4aa2af7c94a1d44c6a84bcc
Jessie Mason <jessiemasonx@hotmail.com>
2019-01-19 15:32:56 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <h1>Mag ik ook nee zeggen?</h1>
     <div class="bodyContainer">
-      <p>Welkom bij Mag Ik Ook Nee Zeggen. <br> Hier ga je door een verhaal, waar jij bepaalt wat er gebeurt. Je komt er achter hoe het is om als burger staande gehouden worden en snel te moeten reageren op verschillende situaties.</p>
+      <p>Welkom bij Mag Ik Ook Nee Zeggen. <br> Hier ga je door een verhaal, waar jij bepaalt wat er gebeurt. Je komt er achter hoe het is om als burger staande gehouden worden en snel te moeten reageren op verschillende situaties.<br> TEST COMMIT TEST HOPENLIJK WERKT HET</p>
       <bigButton
         :text="'start'"
         :to="'/question/1'"/>
